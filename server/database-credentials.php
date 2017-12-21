6f1cb16baee00c0c3b803db24ab5e0c09f3c0d44
Raymart De Guzman <raymart54@gmail.com>
2017-12-21 16:33:15 -0500

    show-map now working

--- server/database-credentials.php
+++ server/database-credentials.php
@@ -1,6 +1,6 @@
 <?php
-$servername="localhost";
-$username= "root";
-$password= "";
-$database= "";
-?>	
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "wifi_database";
+
